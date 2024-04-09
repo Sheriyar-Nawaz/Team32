0d8ce6d51bf4e6059d0d394838182c2f0513f889
MohammedYusufQureshi <mohammed.qureshi.3@city.ac.uk>
2024-04-10 00:24:34 +0100

    Login page updated - 95% complete

--- Version 3.4/src/Login.java
+++ Version 3.4/src/Login.java
@@ -2,44 +2,50 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Array;
 import java.util.HashMap;
 
 public class Login implements ActionListener {
     JFrame frame = new JFrame();
     JButton loginButton = new JButton("Login");
-    JButton resetButton = new JButton("Reset");
-    JTextField userIDField = new JTextField();
+    String[] users = {"","Line Chef", "Sous Chef", "Head Chef"};
+    JComboBox userIDs = new JComboBox(users);
     JPasswordField userPasswordField = new JPasswordField();
-    JLabel userIDLabel = new JLabel("UserID:");
+    JLabel userIDLabel = new JLabel("User:");
     JLabel userPasswordLabel = new JLabel("Password:");
-    JLabel messageLabel = new JLabel();
-    HashMap<String,String> logininfo = new HashMap<String, String>();
-    public Login(HashMap<String,String> loginInfoOriginal) {
-        logininfo = loginInfoOriginal;
+    JLabel logo = new JLabel();
+    ImageIcon image = new ImageIcon("Lancasters-logos.jpeg");
+    HashMap<String,String> logininfo;
+    public Login(HashMap<String,String> logininfo) {
+        this.logininfo = logininfo;
 
-        userIDLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,150,75,25);
+        logo.setIcon(image);
+        logo.setBounds(425,100,200,200);
 
-        messageLabel.setBounds(125,250,250,35);
-        messageLabel.setFont(new Font(null, Font.ITALIC, 25));
 
-        userIDField.setBounds(125,100,200,25);
-        userPasswordField.setBounds(125,150,200,25);
+        userIDLabel.setBounds(330,300,75,25);
+        userIDLabel.setForeground(Color.white);
+        userPasswordLabel.setBounds(300,350,75,25);
+        userPasswordLabel.setForeground(Color.white);
 
-        loginButton.setBounds(125,200,100,25);
+        userIDs.setBounds(375,300,200,25);
+        userIDs.addActionListener(this);
+        userPasswordField.setBounds(375,350,200,25);
+
+        loginButton.setBounds(425,400,100,25);
         loginButton.setFocusable(false);
         loginButton.addActionListener(this);
 
-
-
         frame.add(userIDLabel);
         frame.add(userPasswordLabel);
-        frame.add(messageLabel);
-        frame.add(userIDField);
+        frame.add(logo);
+        frame.add(userIDs);
         frame.add(userPasswordField);
         frame.add(loginButton);
+        frame.getContentPane().setBackground(new Color(43,51,54));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,420);
+        frame.setSize(950,700);
+        frame.setLocationRelativeTo(null);
         frame.setLayout(null);
         frame.setVisible(true);
     }
@@ -48,14 +54,17 @@ public class Login implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         if (e.getSource() == loginButton){
-            String userID = userIDField.getText();
+            String userID = userIDs.getSelectedItem().toString();
             String password = String.valueOf(userPasswordField.getPassword());
 
             if(logininfo.containsKey(userID)){
                 if(logininfo.get(userID).equals(password)){
-                    messageLabel.setForeground(Color.green);
-                    messageLabel.setText("Login successful");
+                    JOptionPane.showMessageDialog(null, "Login Successful", "Success!", JOptionPane.PLAIN_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Incorrect Password", "Error!", JOptionPane.ERROR_MESSAGE);
                 }
+            } else {
+                JOptionPane.showMessageDialog(null, "Please Select a User", "Error!", JOptionPane.ERROR_MESSAGE);
             }
         }
 
