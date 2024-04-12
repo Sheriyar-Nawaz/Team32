94f902f0bd682e737420450d63df68c5e735abcf
Thomas-Beard <thomas.beard@city.ac.uk>
2024-04-12 16:40:29 +0100

    added insert statement example to Driver class

--- Version 3.4/src/Driver.java
+++ Version 3.4/src/Driver.java
@@ -9,15 +9,22 @@ public class Driver {
         String password = "7VQ_A-ZBz2w";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(url, username, password);
 
+            Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Dishes");
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM Dishes");
+//
+//            while(resultSet.next()){
+//                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4));
+//            }
 
-            while(resultSet.next()){
-                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4));
-            }
+            String sql1 = "INSERT into Recipes "
+                    + "(RecipeID, Name, Description, isApproved)"
+                    + "values ('4', 'Test', 'Test_query', '1')";
+
+            statement.executeUpdate(sql1);
+            System.out.println("Insert Complete.");
 
             connection.close();
         }
