3223facf1cac997e7cdc4030638206a91e4a5614
Thomas-Beard <thomas.beard@city.ac.uk>
2024-04-12 19:31:57 +0100

    Working prepareStatement queries. Successfully printed correct outputs from SELECT Recipe Table.

--- Version 3.4/src/Driver.java
+++ Version 3.4/src/Driver.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,14 +8,40 @@ public class Driver {
         String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t32";
         String username = "in2033t32_a";
         String password = "7VQ_A-ZBz2w";
+
+        Statement statement1 = null;
+        PreparedStatement statement2 = null;
+        ResultSet resultSet = null;
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
+            statement1 = connection.createStatement(); // For Simple Queries (creationStatement)
+            statement2 = connection.prepareStatement("SELECT * from Recipes where RecipeID > ? and isApproved = ?"); // Larger Queries (prepareStatement)
+
+            statement2.setInt(1, 1);
+            statement2.setInt(2, 1);
+
+            resultSet = statement2.executeQuery();
+
+            while(resultSet.next()){
+                int recID = resultSet.getInt("RecipeID");
+                String name = resultSet.getString("Name");
+                String desc = resultSet.getString("Description");
+                int isApproved = resultSet.getInt("isApproved");
+
+                System.out.println(recID + " " + name + " " + desc + " " + isApproved);
+            }
 
 
-//            ResultSet resultSet = statement.executeQuery("SELECT * FROM Dishes");
+
+
+
+              /*
+              Small ResultSet Query
+               */
+//            resultSet = statement1.executeQuery("SELECT * FROM Dishes");
 //
 //            while(resultSet.next()){
 //                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4));
@@ -28,7 +55,7 @@ public class Driver {
 //                    + "(RecipeID, Name, Description, isApproved)"
 //                    + "values ('5', 'Test_1', 'Test_DELETE', '1')";
 //
-//            statement.executeUpdate(sql1);
+//            statement1.executeUpdate(sql1);
 //            System.out.println("Insert Complete.");
 
               /*
@@ -38,14 +65,14 @@ public class Driver {
 //                    + "(MenuID, Title, CreationDate)"
 //                    + "values ('2', 'WeekleyMenu2', '2024-04-12')";
 //
-//             statement.executeUpdate(sql2);
+//             statement1.executeUpdate(sql2);
 //             System.out.println("Insert Complete.");
 
 //            String sql3 = "UPDATE MenuDishes "
 //                    + " set MenuID = 2"
 //                    + " where DishID = 3";
 //
-//            statement.executeUpdate(sql3);
+//            statement1.executeUpdate(sql3);
 //            System.out.println("Update Complete.");
 
             /*
@@ -59,10 +86,12 @@ public class Driver {
 
 
 
+
             connection.close();
         }
         catch (Exception e){
             System.out.println(e);
         }
     }
+
 }
