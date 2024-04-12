0ed533015701bea9e3ff1551e3855ff8f3f41f70
Thomas-Beard <thomas.beard@city.ac.uk>
2024-04-12 17:58:50 +0100

    Working Delete SQL Statement Successfully updated Recipe Table in Database

--- Version 3.4/src/Driver.java
+++ Version 3.4/src/Driver.java
@@ -14,37 +14,50 @@ public class Driver {
             Statement statement = connection.createStatement();
 
 
-
 //            ResultSet resultSet = statement.executeQuery("SELECT * FROM Dishes");
 //
 //            while(resultSet.next()){
 //                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4));
 //            }
 
-//            Insert Statement Example:
+
+              /*
+              Insert Example:
+              */
 //            String sql1 = "INSERT into Recipes "
 //                    + "(RecipeID, Name, Description, isApproved)"
-//                    + "values ('4', 'Test', 'Test_query', '1')";
+//                    + "values ('5', 'Test_1', 'Test_DELETE', '1')";
 //
 //            statement.executeUpdate(sql1);
 //            System.out.println("Insert Complete.");
 
               /*
               Insert + Update
-               */
-//            String sql1 = "INSERT into Menus "
+              */
+//            String sql2 = "INSERT into Menus "
 //                    + "(MenuID, Title, CreationDate)"
 //                    + "values ('2', 'WeekleyMenu2', '2024-04-12')";
 //
-//             statement.executeUpdate(sql1);
+//             statement.executeUpdate(sql2);
 //             System.out.println("Insert Complete.");
 
-//            String sql2 = "UPDATE MenuDishes "
+//            String sql3 = "UPDATE MenuDishes "
 //                    + " set MenuID = 2"
 //                    + " where DishID = 3";
 //
-//            statement.executeUpdate(sql2);
+//            statement.executeUpdate(sql3);
 //            System.out.println("Update Complete.");
+
+            /*
+            Delete Example:
+             */
+//            String sql4 = "DELETE from Recipes WHERE RecipeID = 5";
+//            int rowsAffected = statement.executeUpdate(sql4);
+//
+//            System.out.println("Rows Affected: " + rowsAffected);
+//            System.out.println("Delete Complete.");
+
+
 
             connection.close();
         }
