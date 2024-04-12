2c23e224fb8e35817e4e132b328c844da880a103
Thomas-Beard <thomas.beard@city.ac.uk>
2024-04-12 17:13:58 +0100

    Working insert + update sql statements in java. Succesfully inserted new row in Menus table in Database. Successfully updated DishID in MenuDishes table in Database.

--- Version 3.4/src/Driver.java
+++ Version 3.4/src/Driver.java
@@ -13,18 +13,38 @@ public class Driver {
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
 
+
+
 //            ResultSet resultSet = statement.executeQuery("SELECT * FROM Dishes");
 //
 //            while(resultSet.next()){
 //                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getBigDecimal(4));
 //            }
 
-            String sql1 = "INSERT into Recipes "
-                    + "(RecipeID, Name, Description, isApproved)"
-                    + "values ('4', 'Test', 'Test_query', '1')";
+//            Insert Statement Example:
+//            String sql1 = "INSERT into Recipes "
+//                    + "(RecipeID, Name, Description, isApproved)"
+//                    + "values ('4', 'Test', 'Test_query', '1')";
+//
+//            statement.executeUpdate(sql1);
+//            System.out.println("Insert Complete.");
 
-            statement.executeUpdate(sql1);
-            System.out.println("Insert Complete.");
+              /*
+              Insert + Update
+               */
+//            String sql1 = "INSERT into Menus "
+//                    + "(MenuID, Title, CreationDate)"
+//                    + "values ('2', 'WeekleyMenu2', '2024-04-12')";
+//
+//             statement.executeUpdate(sql1);
+//             System.out.println("Insert Complete.");
+
+//            String sql2 = "UPDATE MenuDishes "
+//                    + " set MenuID = 2"
+//                    + " where DishID = 3";
+//
+//            statement.executeUpdate(sql2);
+//            System.out.println("Update Complete.");
 
             connection.close();
         }
