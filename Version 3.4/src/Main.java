eeea11b651476b3f5bff310a5dec1feac7b8f5b9
MohammedYusufQureshi <mohammed.qureshi.3@city.ac.uk>
2024-04-13 14:03:55 +0100

    menu gui updated

--- Version 3.4/src/Main.java
+++ Version 3.4/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args){
         UserPass userPass = new UserPass();
-       // LoginGUI login = new LoginGUI(userPass.getLogininfo());
+        LoginGUI login = new LoginGUI(userPass.getLogininfo());
         //MainMenuGUI m = new MainMenuGUI(null);
-        MenuGUI m = new MenuGUI(null);
+        //MenuGUI m = new MenuGUI(null);
 
     }
 }
--- Version 3.4/src/MenuGUI.java
+++ Version 3.4/src/MenuGUI.java
@@ -7,7 +7,9 @@ import java.util.List;
 import java.util.Objects;
 
 public class MenuGUI extends GUI implements ActionListener {
-    private JComboBox<Object> dishComboBox;
+    private JList<String> dishList;
+    private JList<String> menu;
+    private JComboBox<Object> menuComboBox;
     private JButton addToMenuButton;
     private JButton finalizeMenuButton;
     private List<String> selectedDishes;
@@ -15,28 +17,38 @@ public class MenuGUI extends GUI implements ActionListener {
     public MenuGUI(String user) {
         super(user);
         selectedDishes = new ArrayList<>();
-        JLabel dishLabel = new JLabel("Select Dish:");
+        JLabel dishLabel = new JLabel("Available Dishes");
         dishLabel.setForeground(Color.white);
-        dishLabel.setBounds(250, 300, 100, 25);
+        dishLabel.setBounds(50, 300, 100, 25);
         add(dishLabel);
 
         add(backButton);
         backButton.addActionListener(this);
 
-        logo.setBounds(300,0,300,300);
+        logo.setBounds(270,0,300,300);
         add(logo);
 
-        dishComboBox = new JComboBox<>();
-        dishComboBox.setBounds(325,300,200,25);
-        add(dishComboBox);
+        dishList = new JList<>(new String[]{"Spaghetti", "Salad", "Pizza"});
+        JScrollPane dishScrollPane = new JScrollPane(dishList);
+        dishScrollPane.setBounds(50,325,200,300);
+        dishList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        add(dishScrollPane);
+
+        menu = new JList<>(new String[]{"Spaghetti", "Salad", "Pizza"});
+        menu.setBounds(650,325,200,300);
+        add(menu);
+
+        menuComboBox = new JComboBox<>();
+        menuComboBox.setBounds(50,250,200,25);
+        add(menuComboBox);
 
         addToMenuButton = new JButton("Add to Menu");
-        addToMenuButton.setBounds(350,350,150,25);
+        addToMenuButton.setBounds(350,400,150,25);
         addToMenuButton.addActionListener(this);
         add(addToMenuButton);
 
         finalizeMenuButton = new JButton("Finalize Menu");
-        finalizeMenuButton.setBounds(350,400,150,25);
+        finalizeMenuButton.setBounds(350,450,150,25);
         finalizeMenuButton.addActionListener(this);
         if (!Objects.equals(user, "Head Chef")){
             finalizeMenuButton.setEnabled(false);
@@ -51,12 +63,12 @@ public class MenuGUI extends GUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addToMenuButton) {
-            String selectedDish = (String) dishComboBox.getSelectedItem();
-            selectedDishes.add(selectedDish);
+            //String selectedDish = (String) scrollPane.getSelectedItem();
+           // selectedDishes.add(selectedDish);
             // Here you can add the selected dish to the menu
             // For now, let's just print it
-            System.out.println("Selected dish: " + selectedDish);
-            JOptionPane.showMessageDialog(null, "Dish added to menu: " + selectedDish);
+            //System.out.println("Selected dish: " + selectedDish);
+            //JOptionPane.showMessageDialog(null, "Dish added to menu: " + selectedDish);
         } if (e.getSource() == finalizeMenuButton) {
             if (!selectedDishes.isEmpty()) {
                 // Here you can finalize the menu
@@ -76,9 +88,9 @@ public class MenuGUI extends GUI implements ActionListener {
 
     // Dummy dish data for demonstration
     public void populateDishes(String[] dishNames) {
-        for (String dishName : dishNames) {
-            dishComboBox.addItem(dishName);
-        }
+       // for (String dishName : dishNames) {
+            //dishComboBox.addItem(dishName);
+        //}
     }
 }
 
