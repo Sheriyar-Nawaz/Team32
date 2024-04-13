5eada9552fbc15d8d49f4b38356e63f13df369d8
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2024-04-13 22:14:19 +0100

    Started Writing Classes from Diagram

---
+++ Version 3.4/src/Chef.java
@@ -0,0 +1,13 @@
+public class Chef {
+    private String chefld;
+    private String chefName;
+
+    public void createRecipe(Recipe recipe) {
+    }
+
+    public void reviewRecipe(Recipe recipe) {
+    }
+
+    public void approveRecipe(Recipe recipe) {
+    }
+}
