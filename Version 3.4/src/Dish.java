98971b3d7e24ff6b7fa615ddcea1869569dfd73b
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2024-04-13 22:32:46 +0100

    Create Dish Class

---
+++ Version 3.4/src/Dish.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Dish {
+    private String dishId;
+    private String dishName;
+    private List<Recipe> recipes;
+
+    public Dish(String id, String name, List<Recipe> recipes){
+        this.dishId = id;
+        this.dishName = name;
+        this.recipes = recipes;
+    }
+}
