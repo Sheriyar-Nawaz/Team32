23025c47684d7399bf3f560b7e277d76e7902c40
MohammedYusufQureshi <mohammed.qureshi.3@city.ac.uk>
2024-04-13 23:02:18 +0100

    create menu gui

--- Version 3.4/src/Main.java
+++ Version 3.4/src/Main.java
@@ -4,8 +4,9 @@ public class Main {
         UserPass userPass = new UserPass();
         //LoginGUI login = new LoginGUI(userPass.getLogininfo());
         //MainMenuGUI m = new MainMenuGUI(null);
-//<<<<<<< HEAD
         MenuGUI m = new MenuGUI(null);
+//<<<<<<< HEAD
+
 
 
 //>>>>>>> e277b40984dd885437e530a68fd22473157f477d
