799e89f8a692685ee32ef8dc142b93878d7d51f0
Thomas-Beard <thomas.beard@city.ac.uk>
2024-04-14 20:29:32 +0100

    Updated StartCooking + CompleteCooking methods to both take in dishType as a parameter (Starters, Mains).
    TO-DO: sendMenu method in KitchenManagementSender class which implements the interface KitchenManagementSenderInterface

--- Version 3.4/src/KitchenFrontOfHouseReceiver.java
+++ Version 3.4/src/KitchenFrontOfHouseReceiver.java
@@ -1,9 +1,24 @@
 import java.util.Map;
+import java.sql.*;
+import java.util.List;
+import java.util.Map;
 
-public interface KitchenFrontOfHouseReceiver {
-
+interface KitchenFrontOfHouseReceiverInterface {
   OrderGUI receiveOrder();
   Map<Integer, Boolean> receiveCourseCompletionStatus();
 
+}
 
+public class KitchenFrontOfHouseReceiver implements KitchenFrontOfHouseReceiverInterface{
+
+
+  @Override
+  public OrderGUI receiveOrder() {
+    return null;
+  }
+
+  @Override
+  public Map<Integer, Boolean> receiveCourseCompletionStatus() {
+    return null;
+  }
 }
