9f484155a3009580bf563ab13eb7f03399569fa8
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2024-04-15 00:27:34 +0100

    Order Table Added

--- Version 3.4/src/OrderDB.java
+++ Version 3.4/src/OrderDB.java
@@ -1,3 +1,6 @@
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.*;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,6 +13,35 @@ public class OrderDB {
     Statement statement1 = null;
     PreparedStatement statement2 = null;
     ResultSet resultSet = null;
+    private JTable table_1 = new JTable();
+
+    public JTable getOrders(){
+        //Load all records from stock table. Get quantity recieved for every ingredientID
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            statement1 = connection.createStatement();
+            resultSet = statement1.executeQuery("SELECT OrderID, TableNumber, OrderDateTime, Status FROM Orders");
+            table_1.setModel(DbUtils.resultSetToTableModel(resultSet));
+            connection.close();
+            return table_1;
+/*
+            while(resultSet.next()){
+                int ID = resultSet.getInt("IngredientID");
+                String name = resultSet.getString("Name");
+                double stock = resultSet.getDouble("Stock");
+                System.out.println(ID + " "  + name + " " + stock);
+            }
+ */
+
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+    }
     public void StartCooking(int orderID, String dishType){
         //Select OrderID from orders table and update string status to for example: "Mains: Cooking"
         try {
