bbfe68709df2112e148798f77c296ebc02ff7544
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2024-04-14 20:53:19 +0100

    StockGUI updated

--- Version 3.4/src/StockGUI.java
+++ Version 3.4/src/StockGUI.java
@@ -1,14 +1,11 @@
-import net.proteanit.sql.DbUtils;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.ResultSet;
 
 public class StockGUI extends GUI implements ActionListener {
 
-    private JComboBox<String> ingredientComboBox;
+    private JTextField ingredientField;
     private JButton addToStockButton;
     private JButton addDeliveryButton;
     private JTextField quantityField;
@@ -20,10 +17,6 @@ public class StockGUI extends GUI implements ActionListener {
         logo.setBounds(270,-50,300,300);
         add(logo);
 
-        JLabel ingredientLabel = new JLabel("Select Ingredient:");
-        ingredientLabel.setForeground(Color.white);
-        ingredientLabel.setBounds(300, 400, 150, 25);
-        add(ingredientLabel);
 
         add(backButton);
         backButton.addActionListener(this);
@@ -32,36 +25,36 @@ public class StockGUI extends GUI implements ActionListener {
         table_1 = sdb.getStock();
 
         JScrollPane tableScrollPane = new JScrollPane(table_1);
-        tableScrollPane.setBounds(250,250,350,100);
+        tableScrollPane.setBounds(250,175,350,100);
         table_1.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         add(tableScrollPane);
 
+        JLabel ingredientLabel = new JLabel("Enter Ingredient:");
+        ingredientLabel.setForeground(Color.white);
+        ingredientLabel.setBounds(300, 300, 150, 25);
+        add(ingredientLabel);
 
-        ingredientComboBox = new JComboBox<>();
-        ingredientComboBox.setBounds(410, 400, 200, 25);
-        add(ingredientComboBox);
+        ingredientField = new JTextField();
+        ingredientField.setBounds(410, 300, 200, 25);
+        add(ingredientField);
 
-        ingredientComboBox.addItem("Ingredient 1");
-        ingredientComboBox.addItem("Ingredient 2");
-        ingredientComboBox.addItem("Ingredient 3");
-
-        JLabel quantityLabel = new JLabel("Select quantity:");
+        JLabel quantityLabel = new JLabel("Enter quantity:");
         quantityLabel.setForeground(Color.white);
-        quantityLabel.setBounds(350, 400, 150, 25);
+        quantityLabel.setBounds(300, 350, 150, 25);
         add(quantityLabel);
 
         quantityField = new JTextField();
         quantityField.setForeground(Color.black);
-        quantityField.setBounds(410, 450, 150, 25);
+        quantityField.setBounds(410, 350, 150, 25);
         add(quantityField);
 
         addToStockButton = new JButton("Add to Stock");
-        addToStockButton.setBounds(390, 500, 150, 25);
+        addToStockButton.setBounds(390, 400, 150, 25);
         addToStockButton.addActionListener(this);
         add(addToStockButton);
 
         addDeliveryButton = new JButton("Add delivery");
-        addDeliveryButton.setBounds(390, 400, 150, 25);
+        addDeliveryButton.setBounds(390, 440, 150, 25);
         addDeliveryButton.addActionListener(this);
         add(addDeliveryButton);
 
@@ -72,7 +65,7 @@ public class StockGUI extends GUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addToStockButton) {
-            String selectedIngredient = (String) ingredientComboBox.getSelectedItem();
+            String selectedIngredient = (String) ingredientField.getText();
             String quantityText = (String) quantityField.getText();
             try{
                 int quantity = Integer.parseInt(quantityText); // convert the quantity to an integer
