35219e2699958740dcde52ddfc3b7515e7dd3fd9
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2024-04-15 00:51:37 +0100

    Small typo fix

--- Version 3.4/src/RecipesDB.java
+++ Version 3.4/src/RecipesDB.java
@@ -24,7 +24,7 @@ public class RecipesDB {
         connect();
         Map<Integer, String> recipes = new HashMap<>();
         try {
-            PreparedStatement pstmt = connection.prepareStatement("SELECT RecipeID, Name FROM Recipes WHERE Status = 'Submtted Draft'");
+            PreparedStatement pstmt = connection.prepareStatement("SELECT RecipeID, Name FROM Recipes WHERE Status = 'SubmittedDraft'");
             resultSet = pstmt.executeQuery();
             while (resultSet.next()) {
                 int recipeId = resultSet.getInt("RecipeID");
