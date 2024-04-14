e8d0e8de46cda1e177bfce53726a4d37431fb5b8
Sheri Nawaz <47926276+SheriNawaz@users.noreply.github.com>
2024-04-14 01:19:28 +0100

    Added Method names for wastem db

--- Version 3.4/src/WastemDB.java
+++ Version 3.4/src/WastemDB.java
@@ -1,2 +1,7 @@
 public class WastemDB {
+    public void getIngredients(){}
+    public void addToWaste(String ingredientName){}
+    public void updateStock(String ingredientName){}
+    public void getStock(String ingredientName){}
+    public void updateWaste(){}
 }
