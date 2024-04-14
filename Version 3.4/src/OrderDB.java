a6ed0f37a2cd3165c400503abcb870b09e6aeb79
Thomas-Beard <thomas.beard@city.ac.uk>
2024-04-14 02:41:55 +0100

    added structure for orderDB class methods.

--- Version 3.4/src/OrderDB.java
+++ Version 3.4/src/OrderDB.java
@@ -1,13 +1,58 @@
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class OrderDB {
+    String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t32";
+    String username = "in2033t32_a";
+    String password = "7VQ_A-ZBz2w";
+
+    Statement statement1 = null;
+    PreparedStatement statement2 = null;
+    ResultSet resultSet = null;
     public void StartCooking(){
         //Select OrderID from orders table and update isCooking
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            //TO-DO
+
+            connection.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
     }
+
     public void CompleteCooking(){
         //Select OrderID and update isComplete to be true and isCooking to be false
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            //TO-DO
+
+            connection.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
     }
 
     public void AddOrder(String tableNumber){
         //Insert new orderid into the table.
         // Update tableNumber of that orderID to tableNumber
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, username, password);
+            //TO-DO
+
+            connection.close();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
     }
 }
